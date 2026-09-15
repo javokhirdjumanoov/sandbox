249bb3aa4857164d5e21fc5c8696ed8fbbec0254
User <jumanovjavohir49@gmail.com>
2026-09-16 01:53:34 +0500

    program cs

--- Program.cs
+++ Program.cs
@@ -22,10 +22,12 @@ app.MapGet("/api/users", async (NpgsqlDataSource dataSource) =>
         var items = new List<NameRecord>();
         await using var command = dataSource.CreateCommand("SELECT id, name FROM users ORDER BY id DESC");
         await using var reader = await command.ExecuteReaderAsync();
+        
         while (await reader.ReadAsync())
         {
             items.Add(new NameRecord(reader.GetInt32(0), reader.GetString(1)));
         }
+        
         return Results.Ok(items);
     }
     catch (Exception ex)
